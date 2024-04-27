6d24ced181528cd0c089ed801371f39e07e6dcdb
survi09mukherjee <140266674+survi09mukherjee@users.noreply.github.com>
2024-04-27 11:41:21 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,9 +1,18 @@
-const nav = document.getElementById('main-nav');
-
 window.addEventListener('scroll', function() {
-  if (window.scrollY > 100) { // Adjust the scroll position as needed
-    nav.classList.add('scrolled');
-  } else {
-    nav.classList.remove('scrolled');
-  }
-});
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+      navbar.style.backgroundColor = '#555'; // Change the background color when scrolled
+    } else {
+      navbar.style.backgroundColor = '#333'; // Revert to original color when back at the top
+    }
+  });
+  
+  var menuItems = document.querySelectorAll('#navbar a');
+  menuItems.forEach(function(item) {
+    item.addEventListener('mouseover', function() {
+      this.style.color = '#ff0'; // Change font color on hover
+    });
+    item.addEventListener('mouseout', function() {
+      this.style.color = '#fff'; // Revert font color on mouseout
+    });
+  });
